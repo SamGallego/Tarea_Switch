1aa326d23894f274cc7d919514a1a811e1ee62e9
SamGallego <samuel.gallego15@gmail.com>
2024-11-03 09:36:06 +0100

    Tarea_Switch

---
+++ app/src/main/java/com/example/tarea_switch/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.tarea_switch;
+
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Switch switchButton;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        switchButton = findViewById(R.id.switch1);
+        imageView = findViewById(R.id.imageView);
+
+        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b){
+                    imageView.setVisibility(ImageView.VISIBLE);
+                }else {
+                    imageView.setVisibility(ImageView.GONE);
+                }
+            }
+        });
+    }
+}
